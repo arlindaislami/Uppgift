04fe0733d8c7486a0c247572472ef02c6e5e3d07
arlindaislami <arlindaaislamii@gmail.com>
2024-11-12 22:27:20 +0100

    Uppgift1

--- Uppgift1/ContentView.swift
+++ Uppgift1/ContentView.swift
@@ -9,15 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        VStack(spacing: 0.0) {
+                
+                   HStack(spacing: 0.0) {
+                       
+                       VStack {
+                       }
+                       .frame(maxWidth: .infinity, maxHeight: 130)
+                       .background(Color.red)
+                       
+                       VStack {
+                       }
+                       .frame(maxWidth: .infinity, maxHeight: 130)
+                       .background(Color.green)
+                       
+                       VStack {
+                       }
+                       .frame(maxWidth: .infinity, maxHeight: 130)
+                       .background(Color.yellow)
+                       
+                   }
+            VStack {
+                
+            }
+            .frame(maxWidth: .infinity, maxHeight: 160)
+            .background(Color.gray)
+            .padding(.top, 5)
+                   
+            
+            VStack {
+                
+            }
+            .frame(maxWidth: 100, maxHeight: 100)
+            .background(Color.white)
+            .padding(5)
+                 
+                   
+                   Spacer()
+                   
+            HStack {
+                Spacer()
+                
+                VStack {
+                
+                }
+                .frame(maxWidth: 70, maxHeight: 70)
+                .background(Color.black)
+                .padding()
+            }
+            .frame(maxWidth: .infinity, maxHeight: 100)
+            .background(Color.red)
+                   
+               }
+               .frame(maxHeight: .infinity)
+               .background(Color.blue)
+               
+           }
+       }
 
 #Preview {
     ContentView()
